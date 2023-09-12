94b9409cf237186d41059097f45f82394238967b
Juan Jesús Alejo Sillero <122260827+JuanJesusAlejoSillero@users.noreply.github.com>
2023-09-12 13:20:15 +0200

    Añadida fecha y hora al script

--- apt-updates-telegram.sh
+++ apt-updates-telegram.sh
@@ -6,35 +6,49 @@ BOT_TOKEN="TOKEN_DEL_BOT"
 # ID de tu chat en Telegram (Sustituye 'ID_DEL_CHAT' con tu ID de chat real)
 CHAT_ID="ID_DEL_CHAT"
 
-# FQDN del servidor para enviarlo en los mensajes y así poder identificar fácilmente
+# FQDN del servidor para enviarlo en los mensajes y así poder identificarlo fácilmente
 FQDN=$(hostname -f)
+
+# Función para obtener la fecha y hora actual
+obtener_fecha_hora() {
+    date "+%d/%m/%Y %H:%M:%S"
+}
 
 # Función para enviar un mensaje a través de la API de Bots de Telegram
 enviar_mensaje_telegram() {
     local mensaje="$1"
     curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
-         -d "chat_id=$CHAT_ID" \
-         -d "text=$mensaje" \
-         -d parse_mode="markdown" > /dev/null
+        -d "chat_id=$CHAT_ID" \
+        -d "text=$mensaje" \
+        -d parse_mode="markdown" >/dev/null
 }
+
+# Obtener la fecha y hora actual
+fecha_hora=$(obtener_fecha_hora)
 
 # Comprobar actualizaciones y almacenar el resultado
 actualizaciones=$(apt update 2>&1)
 
 # Comprobar si existe el archivo /var/run/reboot-required
 if [ -e "/var/run/reboot-required" ]; then
-    mensaje="El servidor *$FQDN* necesita ser reiniciado."
+    mensaje="La máquina *$FQDN* necesita ser reiniciada.
+
+Fecha y hora: $fecha_hora"
     enviar_mensaje_telegram "$mensaje"
 fi
 
 # Comprobar si hay actualizaciones disponibles
 if echo "$actualizaciones" | grep -q 'apt list --upgradable'; then
-    mensaje="¡Hay actualizaciones disponibles en *$FQDN*!:
+    mensaje="¡Hay actualizaciones disponibles para *$FQDN*!:
 
-$actualizaciones"
+$actualizaciones
+
+Fecha y hora: $fecha_hora"
 else
     # Si no hay actualizaciones disponibles, enviar un mensaje indicándolo
-    mensaje="No se encontraron actualizaciones disponibles en *$FQDN*."
+    mensaje="No se encontraron actualizaciones disponibles para *$FQDN*.
+
+Fecha y hora: $fecha_hora"
 fi
 
 # Llamar a la función para enviar el mensaje
