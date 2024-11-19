cdd89a077b7fb0d86d931d00a5b9c981ad129efd
Juan Jesús <juanjesusalejosillero@gmail.com>
2024-11-19 16:08:59 +0100

    Incluida la lista de paquetes en el mensaje enviado a Telegram

--- apt-updates-telegram.sh
+++ apt-updates-telegram.sh
@@ -26,6 +26,28 @@ enviar_mensaje_telegram() {
         -d parse_mode="markdown" >/dev/null
 }
 
+# Función para dividir mensajes largos en fragmentos de hasta 20 líneas.
+# La meta es evitar sobrepasar el límite de tamaño que tiene Telegram para los mensajes.
+enviar_mensaje_largo() {
+    local mensaje="$1"
+    local max_lineas=20
+    local lineas=0
+    local mensaje_actual=""
+    while IFS= read -r linea; do
+        mensaje_actual+="$linea"$'\n'
+        ((lineas++))
+        if (( lineas == max_lineas )); then
+            enviar_mensaje_telegram "$mensaje_actual"
+            mensaje_actual=""
+            lineas=0
+        fi
+    done <<< "$mensaje"
+    # Enviar las líneas restantes
+    if [[ -n "$mensaje_actual" ]]; then
+        enviar_mensaje_telegram "$mensaje_actual"
+    fi
+}
+
 # Obtener el idioma del sistema
 system_language=$(echo $LANG | cut -d'_' -f1)
 
@@ -42,11 +64,17 @@ apt update > /dev/null 2>&1
 # Obtener la fecha y hora actual
 fecha_hora=$(obtener_fecha_hora)
 
-# Obtener la cantidad de actualizaciones disponibles
-cantidad_actualizaciones=$(apt list --upgradable 2>/dev/null | grep -c "$keyword")
+# Guardar la salida de apt list --upgradable en una variable para no repetir su ejecución de forma innecesaria
+apt_list_upgradable=$(apt list --upgradable 2>/dev/null)
 
-# Obtener la lista de actualizaciones disponibles
-lista_actualizaciones=$(apt list --upgradable 2>/dev/null | grep '/' | awk -F'/' '{print $2}' | awk '{print $1}' | sort | uniq -c | awk '{print $2 ": " $1}')
+# Obtener la cantidad de actualizaciones disponibles
+cantidad_actualizaciones=$(echo "$apt_list_upgradable" | grep -c "$keyword")
+
+# Obtener la lista detallada de actualizaciones disponibles
+lista_actualizaciones=$(echo "$apt_list_upgradable" | grep "$keyword" | awk -F'/' '{print "- " $1}')
+
+# Obtener la cantidad de actualizaciones por repositorio
+cantidad_por_repo=$(echo "$apt_list_upgradable" | grep '/' | awk -F'/' '{print $2}' | awk '{print $1}' | sort | uniq -c | awk '{print $2 ": " $1}')
 
 # Comprobar si existe el archivo /var/run/reboot-required
 if [ -e "/var/run/reboot-required" ]; then
@@ -61,6 +89,9 @@ if [ "$cantidad_actualizaciones" -gt 0 ]; then
     mensaje="¡Hay *$cantidad_actualizaciones* actualizaciones disponibles para *$FQDN*!
 
 *Cantidad por repositorios:*
+$cantidad_por_repo
+
+*Lista de actualizaciones:*
 $lista_actualizaciones
 
 Fecha y hora: $fecha_hora"
@@ -70,8 +101,8 @@ else
 Fecha y hora: $fecha_hora"
 fi
 
-# Llamar a la función para enviar el mensaje
-enviar_mensaje_telegram "$mensaje"
+# Dividir el mensaje si es necesario y enviarlo
+enviar_mensaje_largo "$mensaje"
 
 # Source:
 # https://github.com/JuanJesusAlejoSillero/apt-updates-telegram
